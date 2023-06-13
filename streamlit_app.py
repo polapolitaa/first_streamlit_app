6ad6c74ffc6f6d76194e13f868a0049a5bf214a0
polapolitaa <49547442+polapolitaa@users.noreply.github.com>
2023-06-14 03:22:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -32,13 +39,10 @@ try:
   if not fruit_choice:
      streamlit.error("Please select a fruit to get information.")
   else:
-      streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      # streamlit.text(fruityvice_response.json())
-      # take the json version of the response and normalize it
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#       streamlit.write('The user entered ', fruit_choice)
+      back_from_function = get_fruity_data(fruit_choice)
       # output response as a table
-      streamlit.dataframe(fruityvice_normalized)
+      streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
